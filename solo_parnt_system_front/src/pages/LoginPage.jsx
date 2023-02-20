4ca5392aaa1b5848dea2ff68b60d353f0d3260a0
austin <villasanamallariaustin@gmail.com>
2023-02-20 23:30:41 +0800

    remove forget password text

--- solo_parnt_system_front/src/pages/LoginPage.jsx
+++ solo_parnt_system_front/src/pages/LoginPage.jsx
@@ -81,16 +81,6 @@ const LoginPage = () => {
                         </div>
                     )}
 
-                    <div className="flex items-center justify-between">
-                        {/* <div className="flex items-center">
-                            <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500" />
-                            <label for="remember-me" className="ml-2 block text-sm text-gray-900">Remember me</label>
-                        </div> */}
-
-                        <div className="text-sm">
-                            <a href="#" className="font-medium text-blue-600 hover:text-blue-500">Forgot your password?</a>
-                        </div>
-                    </div>
 
                     <div>
                         <button type="submit" className="group relative flex w-full justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
